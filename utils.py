d3fa87230172d4c17ee45e8306fbf13e64d8e004
Debjit Karmakar <karmadebjit@gmail.com>
2025-05-06 17:36:05 +0530

    capital gains validation logic

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ from aikar import IncomeTaxCalculator
 from aikar import CapitalGainsCalculator
 
 try:
-    capital_gains_tax = CapitalGainsCalculator('gold', -125000, '3/1/2023', '3/2/2029')
+    capital_gains_tax = CapitalGainsCalculator('equity mutual fund', 125000, '3/1/2023', '3/2/2029')
     print(capital_gains_tax.calculate())
 except Exception as e:
     print("Validation failed:", e)
