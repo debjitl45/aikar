e97c0517959b1b00bb79a30655c64914935f39dd
Debjit Karmakar <karmadebjit@gmail.com>
2025-05-08 09:29:25 +0530

    pypi 1.6v

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aikar',
-    version='0.1.5',
+    version='0.1.6',
     description='Simplifying Indian Taxes with Python',
     author='Debjit Karmakar',
     author_email='karmadebjit@gmail.com',
